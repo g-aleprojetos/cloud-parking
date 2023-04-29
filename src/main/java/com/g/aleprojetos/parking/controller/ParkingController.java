ede407cabba303ec880febe3d04268846ffeb5df
Alexandre dos Santos Gonçalves <g.aleprojetos.Repositories@gmail.com>
2023-04-29 16:37:03 -0300

    Implemeta o swagger no projeto

--- src/main/java/com/g/aleprojetos/parking/controller/ParkingController.java
+++ src/main/java/com/g/aleprojetos/parking/controller/ParkingController.java
@@ -1,17 +1,21 @@
 package com.g.aleprojetos.parking.controller;
 
+import com.g.aleprojetos.parking.controller.dto.ParkingCreateDTO;
 import com.g.aleprojetos.parking.controller.dto.ParkingDTO;
 import com.g.aleprojetos.parking.controller.mapper.ParkingMapper;
 import com.g.aleprojetos.parking.model.Parking;
 import com.g.aleprojetos.parking.service.ParkingService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "Parking Controller")
 public class ParkingController {
 
     private final ParkingService parkingService;
@@ -23,9 +27,27 @@ public class ParkingController {
     }
 
     @GetMapping
-    public List<ParkingDTO> findAll(){
+    @ApiOperation("Find all parking")
+    public ResponseEntity<List<ParkingDTO>> findAll(){
 
        List<Parking> parkingList =  parkingService.findAll();
-        return parkingMapper.toParkingDTOList(parkingList);
+        return ResponseEntity.ok(parkingMapper.toParkingDTOList(parkingList));
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("Find parking for id")
+    public ResponseEntity<ParkingDTO> findById(@PathVariable String id){
+
+        Parking parking =  parkingService.findById(id);
+        return ResponseEntity.ok(parkingMapper.ToParkingDTO(parking));
+    }
+
+    @PostMapping
+    @ApiOperation("Create parking")
+    public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto){
+
+        Parking parkingCreate = parkingMapper.toParkingCreate(dto);
+        Parking parking =  parkingService.create(parkingCreate);
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingMapper.ToParkingDTO(parking));
     }
 }
--- src/main/java/com/g/aleprojetos/parking/service/ParkingService.java
+++ src/main/java/com/g/aleprojetos/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package com.g.aleprojetos.parking.service;
 import com.g.aleprojetos.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Service
@@ -12,8 +13,11 @@ public class ParkingService {
 
     static {
         var id = getUUID();
-        Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+        var id1 = getUUID();
+        Parking parking = new Parking(id, "DMS-1111", "SC", "Celta", "Preto");
+        Parking parking1 = new Parking(id1, "WAS1234", "SP", "VW Gol", "Vermelho");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll(){
@@ -23,5 +27,17 @@ public class ParkingService {
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
     }
 }
