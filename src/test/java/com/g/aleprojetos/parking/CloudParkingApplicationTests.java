95032998b48ba1500ea2b2b353646c98a0b16dc6
Alexandre dos Santos Gonçalves <g.aleprojetos.Repositories@gmail.com>
2023-04-29 11:59:52 -0300

    Estrutura inicial

---
+++ src/test/java/com/g/aleprojetos/parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g.aleprojetos.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
